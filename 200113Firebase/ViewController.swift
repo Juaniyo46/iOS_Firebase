ea3eda15210f80ea7c604dccbff733bb3d16dbbf
Juan Antonio <juan.alvarez@escuelaestech.es>
2020-01-13 16:23:27 +0100

    Initial Commit

---
+++ 200113Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  200113Firebase
+//
+//  Created by juan.alvarez on 13/01/2020.
+//  Copyright © 2020 Juan Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
