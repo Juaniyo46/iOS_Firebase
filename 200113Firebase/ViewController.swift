041c449852cf5dc979db571a044bfbcfb5121ba7
Juan Antonio <juan.alvarez@escuelaestech.es>
2020-01-15 18:01:25 +0100

    metods override in class

--- 200113Firebase/ViewController.swift
+++ 200113Firebase/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mEmail: UITextField!
+    @IBOutlet weak var mPassword: UITextField!
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener{
+            (auth, user) in
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(self.handle!)
+    }
+    
+    
 
-
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    
+    @IBAction func btnRegister(_ sender: Any) {
+    }
+    
+    
 }
 
